2f757278bac9b50c6df75782b98d21574f7bf4b0
Francesco <83709470+FraDepe@users.noreply.github.com>
2023-10-04 17:49:14 +0200

    Re-designed
    Base with speech-recognition (think about change recognizer). getToday now performs two different UrlRequest (one for current day and one for the forecast)

--- main.py
+++ main.py
@@ -8,59 +8,120 @@ from kivymd.app import MDApp
 from kivy.metrics import dp
 from kivymd.uix.tab.tab import MDTabsBase, MDTabs
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.list import OneLineListItem 
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine
 import datetime
 from geopy.geocoders import Nominatim
 import json
+import speech_recognition
+import pyttsx3
+
+
+from kivy import kivy_data_dir
 
 
 
 class MainPage(Screen):
     
-    location = ObjectProperty(None)
     request = None
     city = ""
-
-    def search(self):
-        self.city = self.location.text
-        self.location.text = ""
+    recognizer = speech_recognition.Recognizer()
+    sentence = ""
 
     def listenToSearch(self):
-        pass
+        print("Comincio ad ascoltare")
+        self.ids['microphone'].background_normal = 'media/mic_listening.png' # NON CAMBIA
+        try:
+            with speech_recognition.Microphone() as mic:
+                self.recognizer.adjust_for_ambient_noise(mic)
+                audio = self.recognizer.record(mic, duration=1) # DURATION VA MESSO A 4 SECONDI
+                text = self.recognizer.recognize_google(audio, language="it-it")
+                text = text.lower()
+                self.sentence = text
+                print(text)
+                self.manager.current = 'forecast'
+                self.manager.transition.direction = 'left'
+        except speech_recognition.UnknownValueError:
+            print("Errore")
+            self.sentence = "Roma"                      # NON HO VOGLIA DI PARLARE
+            self.manager.current = 'forecast'           # NON HO VOGLIA DI PARLARE
+            self.manager.transition.direction = 'left'  # NON HO VOGLIA DI PARLARE
+        print("Finisco di ascoltare")
+        self.ids['microphone'].background_normal = 'media/mic2.png' # NON CAMBIA
+
+
+
 
 class ForecastPage(Screen):
 
     city = StringProperty()
+    request_today = None
+    request_forecast = None
+    sentence = ""
 
     def on_enter(self):
-        main_page = self.manager.get_screen('main')
-        self.city = main_page.city
-        gn = Nominatim(user_agent='WeatherApp')
-        coordinates = gn.geocode(self.city)
-        print(f"https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid=c0b583a8bb8b03e64dd0e16bccda95bf")
-        req = UrlRequest(f"https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid=c0b583a8bb8b03e64dd0e16bccda95bf")
-        req.wait()
-        print(req.result)
-        self.request = req
-        return req
+        #analisi della frase per capire se eseguire getToday o getForecast
+        self.getToday()
+        print(kivy_data_dir)
         
+    def getToday(self):
+        print(self.sentence)
+        gn = Nominatim(user_agent='WeatherApp') # Oppure usare l'API di OpenWeather
+        coordinates = gn.geocode(self.sentence)
+        print(f"https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid=c0b583a8bb8b03e64dd0e16bccda95bf&units=metric")
+        req_today = UrlRequest(f"https://api.openweathermap.org/data/2.5/weather?lat={coordinates.latitude}&lon={coordinates.longitude}&appid=c0b583a8bb8b03e64dd0e16bccda95bf&units=metric")
+        req_forecast = UrlRequest(f"https://api.openweathermap.org/data/2.5/forecast?lat={coordinates.latitude}&lon={coordinates.longitude}&appid=c0b583a8bb8b03e64dd0e16bccda95bf&units=metric")
+        req_today.wait()
+        req_forecast.wait()
+        self.ids['today_icon'].source = self.getIcon(req_today.result['weather'][0]['description'])
+        #self.ids['weather_info'].add_widget(
+        #    OneLineListItem(
+        #    text = req.result['weather'][0]['description'] + str(req.result['main']['temp'])
+        #    )
+        #)
+        #print(req.result)
+        self.request_today = req_today
+        self.request_forecast = req_forecast
+        for x in req_forecast.result['list']:
+            print(x['dt'])
+            panel = MDExpansionPanel(
+                content = HourlyForecast(),
+                icon = self.getIcon(x['weather'][0]['description']),
+                panel_cls = MDExpansionPanelOneLine(
+                    text = self.getDay(x['dt'])
+                )
+            )
+            self.ids.forecast_container.add_widget(panel)
 
-    def getIcon(self):
-        main_page = self.manager.get_screen('main')
+    def getForecast(self):
+        pass
 
-        return f"https://openweathermap.org/img/wn/{self.request.result['weather'][0]['icon']}@2x.png"
+    def goBack(self):
+        self.manager.current = 'main'
+        self.manager.transition.direction = 'right'
 
-    def getDay(self,day_to_add):
-        day = datetime.datetime.now()
-        if day_to_add == 0:
-            return "Today"
-        elif day_to_add == 1:
-            return "Tomorrow"
-        else: 
-            return (day + datetime.timedelta(day_to_add)).strftime('%A')
+    def getIcon(self, descritpion):
+        return "media/" + descritpion.replace(" ", "_") + ".png"
+
+    def getDay(self,timestamp):
+        day = datetime.datetime.fromtimestamp(timestamp).strftime("%A")
+        if datetime.datetime.now().strftime("%A") in day:
+            return "Today " + str(datetime.datetime.fromtimestamp(timestamp))[11:16]
+        else:
+            return day + " " + str(datetime.datetime.fromtimestamp(timestamp))[11:16]
     
+
+
+class HourlyForecast(MDBoxLayout):
+    pass
+
+
 
 class PageManager(ScreenManager):
     pass
+
+
 
 
 
@@ -74,10 +135,9 @@ class WeatherApp(MDApp):
 
 
 
-
-
 class Tab(MDFloatLayout, MDTabsBase):
     pass
+
 
 
 
